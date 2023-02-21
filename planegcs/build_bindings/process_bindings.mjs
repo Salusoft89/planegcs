c28f0320ad341b1632d0c98f8042742751a64f26
Mira <athes01@gmail.com>
2023-02-21 14:09:21 +0100

    feat(wrapper-rewrite): move njk files to templates

--- planegcs/build_bindings/process_bindings.mjs
+++ planegcs/build_bindings/process_bindings.mjs
@@ -9,15 +9,15 @@ let fn_constraints = getConstraintFunctions();
 
 // get script cli args
 const args = process.argv.slice(2);
-const input_outputs = arrToNTuples(args, 2).map(([input_file, output_file]) => 
+const input_outputs = arrToNTuples(args, 2).map(([template, output_file]) => 
 ({
-    input_file,
+    template,
     output_file
 }));
 
 
-for (const { input_file, output_file } of input_outputs) {
-    let output_str = nunjucks.render(filePath(input_file), { fn_constraints } );
+for (const { template, output_file } of input_outputs) {
+    let output_str = nunjucks.render(filePath(`templates/${template}`), { fn_constraints } );
 
     fs.writeFileSync(filePath(output_file), output_str, (err) => {
         if (err) throw err;
