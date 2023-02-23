f0baaf2867211c140a81aa1eaae0374491b74c59
Mira <athes01@gmail.com>
2023-02-23 10:29:56 +0100

    chore(wrapper-rewrite): refactor

--- planegcs/build_bindings/process_bindings.mjs
+++ planegcs/build_bindings/process_bindings.mjs
@@ -8,25 +8,19 @@ nunjucks.configure({ autoescape: false })
 const fn_constraints = getConstraintFunctions();
 const enums = getEnums();
 const geom_classes = getGeometryClasses();
-let fn_ts_bindings = null;
+// todo: refactor the deps to Makefile
+let fn_ts_bindings = null; // depends on generating bindings.cpp first
 
 // get script cli args: input_file1 output_file1 input_file2 output_file2 ...
 const args = process.argv.slice(2);
-const input_outputs = arrToNTuples(args, 2).map(([template, output_file]) => 
-({
-    template,
-    output_file
-}));
+const input_outputs = arrToNTuples(args, 2);
 
-
-for (const { template, output_file } of input_outputs) {
-    // todo: refactor the deps to Makefile
+for (const [ template, output_file ] of input_outputs) {
     if (template === 'gcs_system.ts.njk') {
         fn_ts_bindings = getFunctionTypesTypescript();
     }
 
     let output_str = nunjucks.render(filePath(`templates/${template}`), { fn_constraints, enums, fn_ts_bindings, geom_classes } );
-
     fs.writeFileSync(filePath(output_file), output_str, (err) => {
         if (err) throw err;
     });
