fe9360c1bc78434886d92fd60e1aac61480d71f4
Mira <athes01@gmail.com>
2023-03-08 13:03:05 +0100

    chore(bindings): add simple ts types

--- planegcs/build_bindings/utils.ts
+++ planegcs/build_bindings/utils.ts
@@ -1,12 +1,12 @@
 import path from 'path';
 
-export function camelToSnakeCase(str) {
+export function camelToSnakeCase(str: string): string {
     str = str.replace(/([PL]2[PL])/, "$1_");
     return str.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase()
 }
 
 // [1, 2, 3, 4], 2 -> [[1, 2], [3, 4]]
-export function arrToNTuples(arr, n) {
+export function arrToNTuples(arr, n: number) {
     return arr.reduce((acc, arg, i) => {
         if (i % n === 0) {
             acc.push([arg]);
@@ -18,6 +18,6 @@ export function arrToNTuples(arr, n) {
 }
 
 // fix relative paths for nodejs
-export function filePath(fname) {
+export function filePath(fname: string): string {
     return path.dirname(process.argv[1]) + "/" + fname;
 }
