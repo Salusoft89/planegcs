84406595ef3e408c3eb567a0e3b81da89e88c76a
Mira <athes01@gmail.com>
2023-03-08 13:02:49 +0100

    chore(bindings): use one javascript config/build i.e. don't run node in Docker when generating bindings

--- planegcs/build_bindings/process_all.sh
+++ planegcs/build_bindings/process_all.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-npx ts-node render_templates.ts \
+npm exec --prefix planegcs/build_bindings ts-node render_templates.ts \
      gcs_system.cpp.njk ../bindings.cpp \
      gcs_system.ts.njk ../bin/gcs_system.ts \
      gcs_system_mock.ts.njk ../bin/gcs_system_mock.ts \
--- planegcs/build_emscripten.sh
+++ planegcs/build_emscripten.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-pushd build_bindings
-npm install && ./process_all.sh
-# todo: generate gcs_system and gcs_system_mock typescript bindings from generated cpp
-popd
 emcmake cmake .
 make -j $(nproc)
 ls -lh bin
