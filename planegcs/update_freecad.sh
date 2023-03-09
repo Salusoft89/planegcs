033f331f55116a3653a3d6ff9eae7934f2f838b9
Mira <athes01@gmail.com>
2023-03-09 11:17:52 +0100

    feat(update): add --skip-checks and disable /dev/null

--- planegcs/update_freecad.sh
+++ planegcs/update_freecad.sh
@@ -3,14 +3,14 @@
 set -e
 
 # see https://stackoverflow.com/questions/600079/how-do-i-clone-a-subdirectory-only-of-a-git-repository/52269934#52269934
-git clone --depth 1 --filter=tree:0 --sparse https://github.com/FreeCAD/FreeCAD.git &> /dev/null
+git clone --depth 1 --filter=tree:0 --sparse https://github.com/FreeCAD/FreeCAD.git
 pushd FreeCAD
 # the behaviour of this command might vary depending on the git version
-git sparse-checkout set src/*.h src/*.hpp src/Mod/Sketcher/App/planegcs &> /dev/null
+git sparse-checkout set src/*.h src/*.hpp src/Mod/Sketcher/App/planegcs --skip-checks
 popd
 
 # move the planegcs source files
-mv FreeCAD/src/Mod/Sketcher/App/planegcs/* .
+mv FreeCAD/src/Mod/Sketcher/App/planegcs/* . 
 # move some required headers
 mv FreeCAD/src/boost_graph_adjacency_list.hpp .
 mv FreeCAD/src/FCConfig.h .
