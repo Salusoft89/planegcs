702b9582589232cb857d9a27cd5d7aab461f9cf3
Mira <athes01@gmail.com>
2023-03-14 14:26:35 +0100

    linter

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 export { GcsWrapper } from "./sketch/gcs_wrapper";
 export { SketchIndex } from "./sketch/sketch_index";
- 
+
 import ModuleFactory from "./planegcs/bin/planegcs.js";
-import { GcsWrapper } from "./sketch/gcs_wrapper";
+import { GcsWrapper } from "./sketch/gcs_wrapper"; 
 import { SketchIndex } from "./sketch/sketch_index";
-  
-export async function make_gcs_wrapper() {
+
+export async function make_gcs_wrapper() { 
     const module = await ModuleFactory();
     const gcs = new module.GcsSystem(); 
 
