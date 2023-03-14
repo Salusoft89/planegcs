8691fa79bf94250a316bf1f4c4541c268b7a3fcb
Mira <athes01@gmail.com>
2023-03-14 18:09:01 +0100

    fix: change tree-sitter import (works?)

--- build-bindings/parse_cpp.test.ts
+++ build-bindings/parse_cpp.test.ts
@@ -1,19 +1,16 @@
 
-let { params_to_call_string, params_to_definition_string } = require('./parse_cpp');
-// import { params_to_call_string, params_to_definition_string } from "./parse_cpp";
+import { params_to_call_string, params_to_definition_string } from "./parse_cpp";
 
-describe('params_to_definition_string', () => {
-    it('works with different parameter types', () => {
+describe('file', () => {
+    it('params_to_definition_string works with different parameter types', () => {
         expect(params_to_definition_string([ 
             { type: 'Point', identifier: '&x' },
             { type: 'double', identifier: '*y' },
             { type: 'int', identifier: 'f' },
         ])).toEqual('Point &x, int y_param_i, int f');
     });
-});
 
-describe('params_to_call_string', () => {
-    it('works with different parameter types', () => {
+    it('params_to_call_string works with different parameter types', () => {
         expect(params_to_call_string([ 
             { type: 'Point', identifier: '&x' },
             { type: 'double', identifier: '*y' },
