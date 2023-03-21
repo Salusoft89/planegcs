1bc949f06f3239d7d05f760a88b817029f7c9b0a
Mira <athes01@gmail.com>
2023-03-21 15:18:34 +0100

    feat: add optional wasm_path attribute

--- index.ts
+++ index.ts
@@ -5,8 +5,10 @@ import ModuleFactory from "./dist/planegcs";
 import { GcsWrapper } from "./sketch/gcs_wrapper"; 
 import type { SketchIndexBase } from "./sketch/sketch_index";
 
-export async function make_gcs_wrapper<SI extends SketchIndexBase>(sketch_index: SI) { 
-    const module = await ModuleFactory();
+export async function make_gcs_wrapper<SI extends SketchIndexBase>(sketch_index: SI, wasm_path?: string) { 
+    const module = await ModuleFactory(
+        wasm_path ? { locateFile: () => wasm_path } : undefined
+    );
     const gcs = new module.GcsSystem();
 
     return new GcsWrapper(gcs, sketch_index);
