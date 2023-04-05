da56047b641ad782b956bf16299fc91a1a8f13ca
Mira <athes01@gmail.com>
2023-04-05 13:49:41 +0200

    change console overriding to calling console.log this allows testing the console output

--- planegcs/headers/Console.h
+++ planegcs/headers/Console.h
@@ -3,14 +3,20 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <emscripten.h>
 
+EM_JS(void, log_string, (const char* str), {
+    console.log(UTF8ToString(str));
+} );
 class Console {
 public:
     static void Log(const char* format, ...) {
         va_list args;
         va_start(args, format);
-        vprintf(format, args);
+        char buffer[512];
+        vsprintf(buffer, format, args);
         va_end(args);
+        log_string(buffer);
     }
 };
 
