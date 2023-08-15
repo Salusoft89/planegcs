1e69a74bd0a63d237969839a587543084e99e2b5
Mira <athes01@gmail.com>
2023-08-15 11:26:59 +0200

    chore: add exports to index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,20 @@
-export { GcsWrapper } from "./sketch/gcs_wrapper";
 export { Algorithm, SolveStatus, DebugMode, Constraint_Alignment } from "./dist/gcs_system";
+export { type ModuleStatic } from "./dist/planegcs";
+export { type SketchPrimitive, type SketchGeometry, type SketchParam,
+         type SketchPoint, type SketchLine, type SketchCircle,
+         type SketchArc, type SketchEllipse, type SketchArcOfEllipse,
+        is_sketch_constraint, is_sketch_geometry, get_referenced_sketch_params } from "./sketch/sketch_primitive";
+export { type Constraint } from "./dist/constraints";
+export { SketchIndex } from "./sketch/sketch_index";
 
-import ModuleFactory from "./dist/planegcs";
+import init_planegcs_module from "./dist/planegcs";
+export { init_planegcs_module };
+
 import { GcsWrapper } from "./sketch/gcs_wrapper"; 
+export { GcsWrapper };
 
 export async function make_gcs_wrapper(wasm_path?: string) { 
-    const module = await ModuleFactory(
+    const module = await init_planegcs_module(
         wasm_path ? { locateFile: () => wasm_path } : undefined
     );
     const gcs = new module.GcsSystem();
