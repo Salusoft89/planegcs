e4c52d124d23fd0711ef20226abfb69ff84a22e7
Mira <athes01@gmail.com>
2023-08-15 09:54:28 +0200

    feat: utility functions for sketch primitives

--- sketch/sketch_primitive.ts
+++ sketch/sketch_primitive.ts
@@ -75,8 +75,31 @@ export interface SketchParam {
 	value: number;
 }
 
-export function is_sketch_geometry(o: SketchPrimitive | SketchParam): o is SketchGeometry {
-	return ['point', 'line', 'circle', 'arc', 'ellipse', 'arc_of_ellipse'].includes(o.type);
+export function is_sketch_geometry(primitive: SketchPrimitive | SketchParam | undefined): primitive is SketchGeometry {
+	if (primitive === undefined) {
+		return false;
+	}
+	return ['point', 'line', 'circle', 'arc', 'ellipse', 'arc_of_ellipse'].includes(primitive.type);
+}
+
+export function is_sketch_constraint(primitive: SketchPrimitive | SketchParam | undefined): primitive is Constraint {
+	if (primitive === undefined) {
+		return false;
+	}
+	return !is_sketch_geometry(primitive);
+}
+
+export function get_referenced_sketch_params(p: SketchPrimitive): string[] {
+	const params: string[] = [];
+	for (const [key, val] of Object.entries(p)) {
+		if (key === 'type') {
+			continue;
+		}
+		if (typeof val === 'string') {
+			params.push(val);
+		}
+	}
+	return params;
 }
 
 // todo: add SketchHyperbola and SketchArcOfHyperbola
