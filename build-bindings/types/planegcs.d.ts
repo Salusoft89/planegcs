6191f90274b7404b1d4b94d1ce5c05d6297b8d83
Mira <athes01@gmail.com>
2023-09-28 11:04:46 +0200

    fix missing ts types

--- build-bindings/types/planegcs.d.ts
+++ build-bindings/types/planegcs.d.ts
@@ -1,7 +1,9 @@
-import { GcsSystemConstructor } from "../planegcs_dist/gcs_system";
+import { GcsSystemConstructor, DoubleVectorConstructor, IntVectorConstructor } from "../planegcs_dist/gcs_system";
 
 export interface ModuleStatic {
     GcsSystem: GcsSystemConstructor;
+    IntVector: IntVectorConstructor;
+    DoubleVector: DoubleVectorConstructor;
 }
 
 export default function Module(config?: {locateFile: () => string}): Promise<ModuleStatic>;
