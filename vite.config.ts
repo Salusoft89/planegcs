a6dd6188ba3f68ef04a612bdadfeff5eeb3f22a1
Mira <athes01@gmail.com>
2024-05-03 11:51:09 +0200

    fix vitest config after update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   test: {
     // fixes the tree-sitter import error
-    threads: false
+    poolOptions: {
+      threads: {
+        singleThread: true,
+      }
+    }
   },
 })
